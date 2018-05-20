496ec36eef7bc3eea561fcb8a21f1cdf2f45ef27
ksuhr1 <ksuhr@ucsc.edu>
2018-05-20 04:29:04 -0700

    Moved ai functions from human function into ai function and get the maximum utility and index, this works but is stupid because it doesn't block the opponent so I ended up winning

--- Player.py
+++ Player.py
@@ -8,6 +8,14 @@ class AIPlayer:
 
 
     def get_alpha_beta_move(self, board):
+
+        get_utility = self.evaluation_function(board)
+        #return maximum utility value
+        maximum_val = max(get_utility)
+        maximum_index = get_utility.index(maximum_val)
+        return maximum_index
+
+
         #alpha = np.inf
         #beta = -np.inf
 
@@ -85,105 +93,6 @@ class AIPlayer:
         raise NotImplementedError('Whoops I don\'t know what to do')
 
 
-
-
-    def evaluation_function(self, board):
-        """
-        Given the current state of the board, return the scalar value that 
-        represents the evaluation function for the current player
-       
-        INPUTS:
-        board - a numpy array containing the state of the board using the
-                following encoding:
-                - the board maintains its same two dimensions
-                    - row 0 is the top of the board and so is
-                      the last row filled
-                - spaces that are unoccupied are marked as 0
-                - spaces that are occupied by player 1 have a 1 in them
-                - spaces that are occupied by player 2 have a 2 in them
-
-        RETURNS:
-        The utility value for the current board
-        """
-       
-       
-        return 0
-
-
-class RandomPlayer:
-    def __init__(self, player_number):
-        self.player_number = player_number
-        self.type = 'random'
-        self.player_string = 'Player {}:random'.format(player_number)
-
-    def get_move(self, board):
-        """
-        Given the current board state select a random column from the available
-        valid moves.
-
-        INPUTS:
-        board - a numpy array containing the state of the board using the
-                following encoding:
-                - the board maintains its same two dimensions
-                    - row 0 is the top of the board and so is
-                      the last row filled
-                - spaces that are unoccupied are marked as 0
-                - spaces that are occupied by player 1 have a 1 in them
-                - spaces that are occupied by player 2 have a 2 in them
-
-        RETURNS:
-        The 0 based index of the column that represents the next move
-        """
-        valid_cols = []
-        for col in range(board.shape[1]):
-            if 0 in board[:,col]:
-                valid_cols.append(col)
-
-        return np.random.choice(valid_cols)
-
-
-class HumanPlayer:
-    def __init__(self, player_number):
-        self.player_number = player_number
-        self.type = 'human'
-        self.player_string = 'Player {}:human'.format(player_number)
-
-
-
-    def get_move(self, board):
-        """
-        Given the current board state returns the human input for next move
-
-        INPUTS:
-        board - a numpy array containing the state of the board using the
-                following encoding:
-                - the board maintains its same two dimensions
-                    - row 0 is the top of the board and so is
-                      the last row filled
-                - spaces that are unoccupied are marked as 0
-                - spaces that are occupied by player 1 have a 1 in them
-                - spaces that are occupied by player 2 have a 2 in them
-
-        RETURNS:
-        The 0 based index of the column that represents the next move
-        """
-        valid_cols = []
-        for i, col in enumerate(board.T):
-            if 0 in col:
-                valid_cols.append(i)
-#        self.count_values(board,2,1)
-#        boardcopy = board
- #       self.evaluation_function(boardcopy)
-        self.evaluation_function(board)
-        move = int(input('Enter your move: '))
-         #self.evaluation_function(board)
-        # self.count_values(board, 2, 1)
-
-        while move not in valid_cols:
-            print('Column full, choose from:{}'.format(valid_cols))
-            move = int(input('Enter your move: '))
-            
-        return move
     #count_values checks the amount of #4's #3's #2's in a row
     #for the entire board
     def count_values(self, board, num, player_num):
@@ -253,6 +162,24 @@ class HumanPlayer:
           #          print("it won")
     
     def evaluation_function(self, board):
+
+        """
+        Given the current state of the board, return the scalar value that 
+        represents the evaluation function for the current player
+       
+        INPUTS:
+        board - a numpy array containing the state of the board using the
+                following encoding:
+                - the board maintains its same two dimensions
+                    - row 0 is the top of the board and so is
+                      the last row filled
+                - spaces that are unoccupied are marked as 0
+                - spaces that are occupied by player 1 have a 1 in them
+                - spaces that are occupied by player 2 have a 2 in them
+
+        RETURNS:
+        The utility value for the current board
+        """
         boardCopy = board
  #       print("original boardCopy:")
   #      print(boardCopy)
@@ -288,10 +215,75 @@ class HumanPlayer:
                  #   print(board)
                     break
         print(self.player_number)
-        print(utility_list)
+        return(utility_list)
 
 
-        #    self.count_values(board,i,1)
-       # boardCopy = board
-       # print(boardCopy)
-        
+class RandomPlayer:
+    def __init__(self, player_number):
+        self.player_number = player_number
+        self.type = 'random'
+        self.player_string = 'Player {}:random'.format(player_number)
+
+    def get_move(self, board):
+        """
+        Given the current board state select a random column from the available
+        valid moves.
+
+        INPUTS:
+        board - a numpy array containing the state of the board using the
+                following encoding:
+                - the board maintains its same two dimensions
+                    - row 0 is the top of the board and so is
+                      the last row filled
+                - spaces that are unoccupied are marked as 0
+                - spaces that are occupied by player 1 have a 1 in them
+                - spaces that are occupied by player 2 have a 2 in them
+
+        RETURNS:
+        The 0 based index of the column that represents the next move
+        """
+        valid_cols = []
+        for col in range(board.shape[1]):
+            if 0 in board[:,col]:
+                valid_cols.append(col)
+
+        return np.random.choice(valid_cols)
+
+
+class HumanPlayer:
+    def __init__(self, player_number):
+        self.player_number = player_number
+        self.type = 'human'
+        self.player_string = 'Player {}:human'.format(player_number)
+
+
+
+    def get_move(self, board):
+        """
+        Given the current board state returns the human input for next move
+
+        INPUTS:
+        board - a numpy array containing the state of the board using the
+                following encoding:
+                - the board maintains its same two dimensions
+                    - row 0 is the top of the board and so is
+                      the last row filled
+                - spaces that are unoccupied are marked as 0
+                - spaces that are occupied by player 1 have a 1 in them
+                - spaces that are occupied by player 2 have a 2 in them
+
+        RETURNS:
+        The 0 based index of the column that represents the next move
+        """
+        valid_cols = []
+        for i, col in enumerate(board.T):
+            if 0 in col:
+                valid_cols.append(i)
+
+        move = int(input('Enter your move: '))
+        while move not in valid_cols:
+            print('Column full, choose from:{}'.format(valid_cols))
+            move = int(input('Enter your move: '))
+            
+        return move
+
